16abfa84ee852dc7995ecf42a87b1a35db3863d6
believer-24 <savdiyatushar17@gmail.com>
2024-06-15 22:03:14 +0530

    2 files

---
+++ age.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=18;
+    int age;
+    
+    cout<<"enter age :";
+    cin>>age;
+    if(age>=a){
+        cout<<"eligible";
+        }
+        else{
+            cout<<"not eligible";
+        }
+        
+        return 0;   
+}
